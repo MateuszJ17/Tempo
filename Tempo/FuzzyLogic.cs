e2194978c8cd583d45422a2b93d898d824116d6a
Janusz Kaniewski <janusz.kaniewski@outlook.com>
2018-12-17 00:51:30 +0100

    Fixed fuzzy logic, added simulation speed slider, added simulation timer, diplaying outside temperature depending on hour of timer (to be changed maybe), getting heat power from right slider input (FOR TEST ONLY!)

--- Tempo/FuzzyLogic.cs
+++ Tempo/FuzzyLogic.cs
@@ -6,24 +6,33 @@ using System.Threading.Tasks;
 
 namespace Tempo
 {
+    /// <summary>
+    /// Fuzzy Logic core of heating system
+    /// </summary>
     public class FuzzyLogic
     {
+        /// <summary>
+        /// Gets power of heating system based on input
+        /// </summary>
         public double GetPower(int input)
         {
-            double[,] X = new double[4, 41]; //temperatura
-            double[,] Y = new double[4, 41]; //moc pieca
+            int Arraylength = 61;
 
-            int Arraylength = X.GetLength(1);
+            double[,] X = new double[5, Arraylength]; //temperatura
+            double[,] Y = new double[5, Arraylength]; //moc pieca
 
-            double[,] wnioskowanie_Y = new double[4, 41];
+            double[,] wnioskowanie_Y = new double[5, Arraylength];
 
-            double[,] agregacja_Y = new double[2, 41];
+            double[,] agregacja_Y = new double[2, Arraylength];
 
-            double uA, uB, uC, uD, uE, uF;
+            //double uA, uB, uC, uD, uE, uF;
+
+            double tempL, tempM, tempMH, tempH;
+            double heatL, heatM, heatMH, heatH;
             double a, x0, x1, x2, b, x;
 
             double zmienna_wejsciowa;
-            double roz_uA = 0, roz_uB = 0, roz_uC = 0;
+            double roz_tempL = 0, roz_tempM = 0, roz_tempMH = 0, roz_tempH = 0;
 
             double srodek_ciezkosci;
             double gora = 0, dol = 0;
@@ -43,82 +52,114 @@ namespace Tempo
 
             for (int i = 0; i < Arraylength; i++)
             {
-                x = (i * 0.5) + 10;
+                x = (i * 0.5) - 15;
                 X[0, i] = x;
 
-                //trójkąt uA - temperatura niska
-                a = 10;
-                x0 = 10;
-                b = 20;
+                //trapez tempL - temperatura niska
+                a = -15;
+                x1 = -15;
+                x2 = 0;
+                b = 5;
+
+                if (a <= x && x <= x1)
+                {
+                    double temp = x1 - a;
+                    if (temp != 0)
+                    {
+                        tempL = (x - a) / (x1 - a);
+                    }
+                    else
+                    {
+                        tempL = 1;
+                    }
+                }
+                else if (x1 <= x && x <= x2)
+                {
+                    tempL = 1;
+                }
+                else if (x2 <= x && x <= b)
+                {
+                    tempL = (b - x) / (b - x2);
+                }
+                else
+                {
+                    tempL = 0;
+                }
+
+                //trójkąt tempM - temperatura średnia
+                a = 0;
+                x0 = 5;
+                b = 10;
 
                 if (a <= x && x <= x0)
                 {
                     double temp = x0 - a;
                     if (temp != 0)
                     {
-                        uA = (x - a) / (x0 - a);
+                        tempM = (x - a) / (x0 - a);
                     }
                     else
                     {
-                        uA = 1;
+                        tempM = 1;
                     }
                 }
                 else if (x0 <= x && x <= b)
                 {
-                    uA = (b - x) / (b - x0);
+                    tempM = (b - x) / (b - x0);
                 }
                 else
                 {
-                    uA = 0;
+                    tempM = 0;
                 }
 
-                //trójkąt uB - temperatura średnia
-                a = 15;
-                x0 = 20;
-                b = 25;
+                //trójkąt tempMH - temperatura średnio-wysoka
+                a = 5;
+                x0 = 10;
+                b = 15;
 
                 if (a <= x && x <= x0)
                 {
-                    uB = (x - a) / (x0 - a);
+                    tempMH = (x - a) / (x0 - a);
                 }
                 else if (x0 <= x && x <= b)
                 {
-                    uB = (b - x) / (b - x0);
+                    tempMH = (b - x) / (b - x0);
                 }
                 else
                 {
-                    uB = 0;
+                    tempMH = 0;
                 }
 
-                //trójkąt uC - temperatura wysoka
-                a = 20;
-                x0 = 30;
-                b = 30;
+                //trójkąt tempH - temperatura wysoka
+                a = 10;
+                x0 = 15;
+                b = 15;
 
                 if (a <= x && x <= x0)
                 {
-                    uC = (x - a) / (x0 - a);
+                    tempH = (x - a) / (x0 - a);
                 }
                 else if (x0 <= x && x <= b)
                 {
                     double temp = b - x0;
                     if (temp != 0)
                     {
-                        uC = (b - x) / (b - x0);
+                        tempH = (b - x) / (b - x0);
                     }
                     else
                     {
-                        uC = 1;
+                        tempH = 1;
                     }
                 }
                 else
                 {
-                    uC = 0;
+                    tempH = 0;
                 }
 
-                X[1, i] = uA;
-                X[2, i] = uB;
-                X[3, i] = uC;
+                X[1, i] = tempL;
+                X[2, i] = tempM;
+                X[3, i] = tempMH;
+                X[4, i] = tempH;
             }
 
             //---------------------------------------------------------------------------
@@ -126,92 +167,111 @@ namespace Tempo
 
             for (int i = 0; i < Arraylength; i++)
             {
-                x = (i * 4.5) - 40;
+                x = (i * 2.63934426) - 30;
                 Y[0, i] = x;
 
-                //trapez uD - moc pieca niska
-                a = -40;
-                x1 = -20;
-                x2 = 20;
-                b = 40;
+                //trapez heatL - moc pieca niska
+                a = -30;
+                x1 = -10;
+                x2 = 10;
+                b = 30;
 
                 if (a <= x && x <= x1)
                 {
                     double temp = x1 - a;
                     if (temp != 0)
                     {
-                        uD = (x - a) / (x1 - a);
+                        heatL = (x - a) / (x1 - a);
                     }
                     else
                     {
-                        uD = 1;
+                        heatL = 1;
                     }
                 }
                 else if (x1 <= x && x <= x2)
                 {
-                    uD = 1;
+                    heatL = 1;
                 }
                 else if (x2 <= x && x <= b)
                 {
-                    uD = (b - x) / (b - x2);
+                    heatL = (b - x) / (b - x2);
                 }
                 else
                 {
-                    uD = 0;
+                    heatL = 0;
                 }
 
-                //trójkąt uE - moc pieca średnia
-                a = 30;
-                x0 = 50;
-                b = 70;
+                //trójkąt heatM - moc pieca średnia
+                a = 20;
+                x0 = 40;
+                b = 60;
 
                 if (a <= x && x <= x0)
                 {
-                    uE = (x - a) / (x0 - a);
+                    heatM = (x - a) / (x0 - a);
                 }
                 else if (x0 <= x && x <= b)
                 {
-                    uE = (b - x) / (b - x0);
+                    heatM = (b - x) / (b - x0);
                 }
                 else
                 {
-                    uE = 0;
+                    heatM = 0;
                 }
 
-                //trapez uF - moc pieca wysoka
-                a = 60;
-                x1 = 80;
-                x2 = 120;
-                b = 140;
+                //trójkąt heatMH - moc pieca średnio-wysoka
+                a = 40;
+                x0 = 60;
+                b = 80;
+
+                if (a <= x && x <= x0)
+                {
+                    heatMH = (x - a) / (x0 - a);
+                }
+                else if (x0 <= x && x <= b)
+                {
+                    heatMH = (b - x) / (b - x0);
+                }
+                else
+                {
+                    heatMH = 0;
+                }
+
+                //trapez heatH - moc pieca wysoka
+                a = 70;
+                x1 = 90;
+                x2 = 110;
+                b = 130;
 
                 if (a <= x && x <= x1)
                 {
-                    uF = (x - a) / (x1 - a);
+                    heatH = (x - a) / (x1 - a);
                 }
                 else if (x1 <= x && x <= x2)
                 {
-                    uF = 1;
+                    heatH = 1;
                 }
                 else if (x2 <= x && x <= b)
                 {
                     double temp = b - x2;
                     if (temp != 0)
                     {
-                        uF = (b - x) / (b - x2);
+                        heatH = (b - x) / (b - x2);
                     }
                     else
                     {
-                        uF = 1;
+                        heatH = 1;
                     }
                 }
                 else
                 {
-                    uF = 0;
+                    heatH = 0;
                 }
 
-                Y[1, i] = uD;
-                Y[2, i] = uE;
-                Y[3, i] = uF;
+                Y[1, i] = heatL;
+                Y[2, i] = heatM;
+                Y[3, i] = heatMH;
+                Y[4, i] = heatH;
             }
 
             zmienna_wejsciowa = input;
@@ -220,52 +280,60 @@ namespace Tempo
             {
                 if (X[0, i] == zmienna_wejsciowa)
                 {
-                    roz_uA = X[1, i];
-                    roz_uB = X[2, i];
-                    roz_uC = X[3, i];
+                    roz_tempL = X[1, i];
+                    roz_tempM = X[2, i];
+                    roz_tempMH = X[3, i];
+                    roz_tempH = X[4, i];
                 }
             }
 
-            //REGUŁY:
-            //R1: Jeżeli x=A to y=F - temperatura niska -> ogrzewanie wysokie
-            //R2: Jeżeli x=B to y=E - temperatura średnia -> ogrzewanie średnie
-            //R3: Jeżeli x=C to y=D - temperatura wysoka -> ogrzewanie niskie
+            //----wnioskowanie
 
             for (int i = 0; i < Arraylength; i++)
             {
-                wnioskowanie_Y[0, i] = (i * 4.5) - 40;
-                agregacja_Y[0, i] = (i * 4.5) - 40;
+                wnioskowanie_Y[0, i] = (i * 2.63934426) - 30;
+                agregacja_Y[0, i] = (i * 2.63934426) - 30;
 
-                if (Y[3, i] > roz_uA) //uF do uA
+                if (Y[1, i] > roz_tempL) //tempL do heatL
                 {
-                    wnioskowanie_Y[3, i] = roz_uA;
-                }
-                else
-                {
-                    wnioskowanie_Y[3, i] = Y[3, i];
-                }
-
-                if (Y[2, i] > roz_uB) //uE do uB
-                {
-                    wnioskowanie_Y[2, i] = roz_uB;
-                }
-                else
-                {
-                    wnioskowanie_Y[2, i] = Y[2, i];
-                }
-
-                if (Y[1, i] > roz_uC) //uD do uC
-                {
-                    wnioskowanie_Y[1, i] = roz_uC;
+                    wnioskowanie_Y[1, i] = roz_tempL;
                 }
                 else
                 {
                     wnioskowanie_Y[1, i] = Y[1, i];
                 }
 
-                double temp;
+                if (Y[2, i] > roz_tempM) //tempM do heatM
+                {
+                    wnioskowanie_Y[2, i] = roz_tempM;
+                }
+                else
+                {
+                    wnioskowanie_Y[2, i] = Y[2, i];
+                }
+
+                if (Y[3, i] > roz_tempMH) //tempMH do heatMH
+                {
+                    wnioskowanie_Y[3, i] = roz_tempMH;
+                }
+                else
+                {
+                    wnioskowanie_Y[3, i] = Y[3, i];
+                }
+
+                if (Y[4, i] > roz_tempH) //tempH do heatH
+                {
+                    wnioskowanie_Y[4, i] = roz_tempH;
+                }
+                else
+                {
+                    wnioskowanie_Y[4, i] = Y[4, i];
+                }
+
+                double temp, temp2;
                 temp = Math.Max(wnioskowanie_Y[1, i], wnioskowanie_Y[2, i]);
-                agregacja_Y[1, i] = Math.Max(temp, wnioskowanie_Y[3, i]);
+                temp2 = Math.Max(temp, wnioskowanie_Y[3, i]);
+                agregacja_Y[1, i] = Math.Max(temp2, wnioskowanie_Y[4, i]);
 
                 gora += agregacja_Y[1, i] * agregacja_Y[0, i];
                 dol += agregacja_Y[1, i];
--- Tempo/MainWindow.xaml.cs
+++ Tempo/MainWindow.xaml.cs
@@ -25,33 +25,77 @@ namespace Tempo
         Temperature temp = new Temperature();
         WeatherData weatherData = new WeatherData();
         DispatcherTimer dt = new DispatcherTimer();
+        FuzzyLogic fuzzyLogic = new FuzzyLogic();
+
+        DateTime timer = new DateTime(2018, 1, 1, 0, 0, 0);
+
+        int speed = 1000;
+        int targetTemp;
         int counter = 1;
+        double power;
 
         public MainWindow()
         {
             InitializeComponent();
             temp.ReadText();
             temp.ParseTemperature();
+            TargetTemperatureValue.Content = TemperatureSlider.Value + "°C";
+            TemperatureSlider.ValueChanged += TemperatureSlider_ValueChanged;
+            targetTemp = int.Parse(TemperatureSlider.Value.ToString());
+            SpeedSlider.Value = 1000;
+            SpeedSlider.ValueChanged += SpeedSlider_ValueChanged;
         }
 
-        private void WeatherBtn_Click(object sender, RoutedEventArgs e)
+        private void SpeedSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            InsideTemperatureLabelValue.Dispatcher.Invoke(new Action(() => 
-            {
-                dt.Interval = TimeSpan.FromSeconds(1);
-                dt.Tick += Dt_Tick;
-                dt.Start();
-            })
-            );
+            speed = int.Parse(Math.Round(SpeedSlider.Value).ToString());
         }
 
-        private void Dt_Tick(object sender, EventArgs e)
+        private void TemperatureSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            //foreach (var item in temp.ParsedTemperatures)
+            TargetTemperatureValue.Content = TemperatureSlider.Value + "°C";
+            targetTemp = int.Parse(TemperatureSlider.Value.ToString());
+            power = fuzzyLogic.GetPower(targetTemp);
+            PowerValue.Content = Math.Round(power).ToString() + "%";
+            HeatingPowerBar.Value = Math.Round(power);
+        }
+
+        private async void WeatherBtn_Click(object sender, RoutedEventArgs e)
+        {
+            await UpdateTimerAndTemp();
+            //InsideTemperatureLabelValue.Dispatcher.Invoke(new Action(() => 
             //{
-            //    OutsideTemperatureLabelValue.Content = item.ToString();
-            //}
-            InsideTemperatureLabelValue.Content = DateTime.Now.ToLongTimeString();
+            //    dt.Interval = TimeSpan.FromSeconds(1);
+            //    dt.Tick += Dt_Tick;
+            //    dt.Start();
+            //})
+            //);
+        }
+
+        //private void Dt_Tick(object sender, EventArgs e)
+        //{
+        //    //foreach (var item in temp.ParsedTemperatures)
+        //    //{
+        //    //    OutsideTemperatureLabelValue.Content = item.ToString();
+        //    //}
+        //    TimeLabelValue.Content = DateTime.Now.ToLongTimeString();
+        //}
+
+
+        private async Task UpdateTimerAndTemp()
+        {
+            string time = "";
+            var temperatures = temp.ParsedTemperatures;
+
+            while (time != "23:59")
+            {
+                OutsideTemperatureLabelValue.Content = temperatures[timer.Hour] + "°C";
+                time = timer.ToString("HH:mm");
+                TimeLabelValue.Content = time;
+                await Task.Delay(speed);
+                timer = timer.AddMinutes(1);   
+            }
+            MessageBox.Show("Symulacja została zakończona", "Koniec symulacji");
         }
 
         private void OptionsButton_Click(object sender, RoutedEventArgs e)
